efeeb2d0374f10ec2312b6f6df2428cd32264704
Dr-hira4 <163442945+Dr-hira4@users.noreply.github.com>
2025-04-20 15:35:33 +0500

    Add files via upload

---
+++ madlibsgame.py
@@ -0,0 +1,13 @@
+# take input from user
+name = input("Enter player name:")
+programming_language = input("Enter a programming language: (e.g: python , next js < typescript , html): ")
+mentor = input("Enter the mentor name: ")
+location = input("Enter location")
+
+# story created
+print("\n Here is my little story based on mad libs game!")
+print(f"once upon a time, there was a boy named {name}.")
+print(f"{name} was very curious and always want to learn new things")
+print(f"One day he decided to learn {programming_language} language at {location}!")
+print(f"Luckily {name} found a great mentor name as {mentor} who was an expert in {programming_language}.")
+print("And so his journey continues...!")
